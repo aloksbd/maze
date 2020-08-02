84c15a63d2d1ee2b9c96fd080670576fa13ffc45
aloksbd <30898579+aloksbd@users.noreply.github.com>
2020-08-02 21:33:08 +0545

    rewind mechanism

--- scripts/blue.gd
+++ scripts/blue.gd
@@ -2,21 +2,33 @@ extends KinematicBody2D
 
 const max_speed = 80
 var velocity = Vector2()
+var input_vector = Vector2.ZERO
 
 onready var animationState = $AnimationTree.get("parameters/playback")
 
 func _physics_process(delta):
-	var input_vector = Vector2.ZERO
-	input_vector.x = -Input.get_action_strength("ui_right") + Input.get_action_strength("ui_left")
-	input_vector.y = -Input.get_action_strength("ui_down") + Input.get_action_strength("ui_up")
+	
+	if Input.is_action_just_pressed("ui_accept"):
+		$rewind.rewind()
+	
+	if $rewind.state == 0:
+		input_vector.x = -Input.get_action_strength("ui_right") + Input.get_action_strength("ui_left")
+		input_vector.y = -Input.get_action_strength("ui_down") + Input.get_action_strength("ui_up")
+		$rewind.record(input_vector)
 	
 	if input_vector != Vector2.ZERO:
 		$AnimationTree.set("parameters/idle/blend_position",input_vector)
 		$AnimationTree.set("parameters/walk/blend_position",input_vector)
 		animationState.travel("walk")
 		velocity = input_vector * max_speed
+		if $rewind.state == 1:
+			velocity = input_vector * -max_speed
 	else:
 		animationState.travel("idle")
 		velocity = Vector2.ZERO
 	
 	move_and_slide(velocity)
+	input_vector = Vector2.ZERO
+
+func moveTo(move):
+	input_vector = move
--- scripts/rewind.gd
+++ scripts/rewind.gd
@@ -1,11 +1,28 @@
 extends Node2D
 
 enum{
-	record,
-	replay
+	RECORD,
+	REWIND
 }
 
-var state = record
+var state = RECORD
+var moves = []
 
-func record(position,move):
-	pass
+func record(move):
+	moves.append(move)
+
+func rewind():
+	state = REWIND
+	yield(get_tree().create_timer(3), "timeout")
+	moves.clear()
+	state = RECORD
+
+func _physics_process(delta):
+	if state == RECORD:
+		return
+	
+	var move = moves.pop_back()
+	if move == null:
+		move = Vector2.ZERO
+	get_parent().moveTo(move)
+	
