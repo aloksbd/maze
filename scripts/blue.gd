28ef32333f29c8f462bb31626613cbbcaac2f39d
aloksbd <30898579+aloksbd@users.noreply.github.com>
2020-08-02 20:46:18 +0545

    backward movement

--- scripts/blue.gd
+++ scripts/blue.gd
@@ -7,8 +7,8 @@ onready var animationState = $AnimationTree.get("parameters/playback")
 
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
-	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector.x = -Input.get_action_strength("ui_right") + Input.get_action_strength("ui_left")
+	input_vector.y = -Input.get_action_strength("ui_down") + Input.get_action_strength("ui_up")
 	
 	if input_vector != Vector2.ZERO:
 		$AnimationTree.set("parameters/idle/blend_position",input_vector)
---
+++ scripts/rewind.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+enum{
+	record,
+	replay
+}
+
+var state = record
+
+func record(position,move):
+	pass
