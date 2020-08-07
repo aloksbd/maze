e62ad776d9fff660679f273ee8fd0c29dbf9f30a
fukhash <juanluiz@yahoo.com>
2020-08-07 10:16:31 -0300

    newmaze

--- scripts/blue.gd
+++ scripts/blue.gd
@@ -23,9 +23,9 @@ func _physics_process(delta):
 		$AnimationTree.set("parameters/walk/blend_position",animation_vector)
 		animationState.travel("walk")
 		input_vector = input_vector.normalized()
-		velocity = input_vector * max_speed
+		velocity = input_vector * -max_speed
 		if $rewind.state == 1:
-			velocity = input_vector * -max_speed
+			velocity = input_vector * max_speed
 			if $rewind.dontMove:
 				velocity = Vector2.ZERO
 	else:
